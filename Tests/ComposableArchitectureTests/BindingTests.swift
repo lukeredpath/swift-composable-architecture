a6472a11d339b93d5cb3881bbacf36989e60d3b3
Luke Redpath <luke@lukeredpath.co.uk>
2021-10-06 10:59:27 +0100

    Add failing test for bug.

--- Tests/ComposableArchitectureTests/BindingTests.swift
+++ Tests/ComposableArchitectureTests/BindingTests.swift
@@ -35,5 +35,62 @@
 
       XCTAssertNoDifference(viewStore.state, .init(nested: .init(field: "Hello!")))
     }
+
+    func testBindingActionWithPullback() {
+      struct State: Equatable {
+        @BindableState var value: String
+        var count: Int = 0
+
+        var viewState: ViewState {
+          get { .init(value: value) }
+          set { value = newValue.value }
+        }
+      }
+
+      enum Action: BindableAction, Equatable {
+        case binding(BindingAction<State>)
+      }
+
+      let reducer = Reducer<State, Action, ()> { state, action, _ in
+        switch action {
+        case .binding(\.$value):
+          state.count += 1
+          return .none
+        default:
+          return .none
+        }
+      }
+      .binding()
+
+      struct ViewState: Equatable {
+        @BindableState var value: String
+      }
+
+      enum ViewAction: BindableAction, Equatable {
+        case binding(BindingAction<ViewState>)
+      }
+
+      let store = Store(initialState: .init(value: "a"), reducer: reducer, environment: ())
+      let viewStore = ViewStore(store)
+
+      let childStore: Store<ViewState, ViewAction> = store.scope(
+        state: \.viewState,
+        action: { viewAction in
+          switch viewAction {
+          case let .binding(action):
+            return .binding(action.pullback(\.viewState))
+          }
+        }
+      )
+      let childViewStore = ViewStore(childStore)
+
+      viewStore.binding(\.$value).wrappedValue = "b"
+      XCTAssertNoDifference(viewStore.state.value, "b")
+      XCTAssertNoDifference(viewStore.count, 1)
+
+      childViewStore.binding(\.$value).wrappedValue = "c"
+      XCTAssertNoDifference(viewStore.state.value, "c")
+      XCTAssertNoDifference(viewStore.count, 2)
+    }
   }
 #endif
