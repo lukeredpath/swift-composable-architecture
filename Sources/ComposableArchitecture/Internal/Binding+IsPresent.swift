165b163b0799c7948c2c7f0799b04dec0cdcc75d
Luke Redpath <lredpath@community.com>
2022-05-14 18:44:05 +0100

    more cleanup

--- Sources/ComposableArchitecture/Internal/Binding+IsPresent.swift
+++ Sources/ComposableArchitecture/Internal/Binding+IsPresent.swift
@@ -3,9 +3,7 @@ import SwiftUI
 extension Binding {
   func isPresent<Wrapped>() -> Binding<Bool> where Value == Wrapped? {
     .init(
-      get: {
-          self.wrappedValue != nil
-      },
+      get: { self.wrappedValue != nil },
       set: { isPresent, transaction in
         guard !isPresent else { return }
         self.transaction(transaction).wrappedValue = nil
